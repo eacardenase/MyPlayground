bfb05827cbc7eac4166ca9d4a49bc2d3347f7a63
Edwin Cardenas <eacardenase@gmail.com>
2024-12-23 15:01:42 -0500

    feat: review structs

--- Contents.swift
+++ Contents.swift
@@ -874,7 +874,66 @@ import UIKit
 //let roslin = Employee(name: "Laura Roslin")
 //let anon = Employee()
 
+//struct Person {
+//    var name: String
+//    
+//    init(name: String) {
+//        print("\(name) was born!")
+//        
+//        self.name = name
+//    }
+//}
 
+//struct FamilyTree {
+//    init() {
+//        print("Creating family tree!")
+//    }
+//}
+//
+//struct Person {
+//    var name: String
+//    lazy var familyTree = FamilyTree()
+//    
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//var ed = Person(name: "Ed")
+//ed.familyTree
+
+//struct Student {
+//    static var classSize = 0
+//    var name: String
+//    
+//    init(name: String) {
+//        self.name = name
+//        Student.classSize += 1
+////        Self.classSize += 1
+//    }
+//}
+//
+//let ed = Student(name: "Ed")
+//let taylor = Student(name: "Taylor")
+//
+//print(Student.classSize)
+
+//struct Person {
+//    private var id: String
+//    
+//    init(id: String) {
+//        self.id = id
+//    }
+//    
+//    func identify() -> String {
+//        "My social security number is \(id)"
+//    }
+//}
+//
+//let ed = Person(id: "12345")
+//ed.identify()
+
+//ed.id // compilation error
 
 //func printName(name: String) -> Void {
 //    print(name)
