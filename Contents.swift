c51aae7dbe337dcb176990ebc9db794569ce6aa4
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 19:31:02 -0500

    feat: optionals and type aliasing

--- Contents.swift
+++ Contents.swift
@@ -1129,23 +1129,208 @@ import UIKit
 //pythons.summarize()
 //beatles.summarize()
 
-protocol Identifiable {
-    var id: String { get set }
-    func identify()
+//protocol Identifiable {
+//    var id: String { get set }
+//    func identify()
+//}
+//
+//extension Identifiable {
+//    func identify() {
+//        print("My ID is \(id).")
+//    }
+//}
+//
+//struct User: Identifiable {
+//    var id: String
+//}
+//
+//let eacardenase = User(id: "eacardenase")
+//eacardenase.identify()
+
+// Optionals
+
+//var age: Int? = nil
+//age = 28
+//
+//print(age)
+
+//var name: String? = nil
+
+//if let unwrapped = name {
+//    print("\(unwrapped.count) letters")
+//} else {
+//    print("Missing name.")
+//}
+
+//if let name {
+//    print("\(name.count) letters")
+//} else {
+//    print("Missing name.")
+//}
+
+//func getUsername() -> String? {
+//    "Taylor Swift"
+//}
+//
+//if let username = getUsername() {
+//    print("Username is \(username)")
+//} else {
+//    print("No username")
+//}
+
+//func greet(_ name: String?) {
+//    guard let unwrapped = name else {
+//        print("You didn't provide a name!")
+//        return
+//    }
+//    
+//    print("Hello, \(unwrapped)!")
+//}
+//
+//greet("Edwin")
+//greet(nil)
+
+//let str = "6"
+//let num = Int(str)!
+//
+//print(num)
+
+//enum Direction: CaseIterable {
+//    case north, south, east, west
+//}
+//
+//let randomDirection = Direction.allCases.randomElement()!
+//print(randomDirection)
+
+//enum Direction: CaseIterable {
+//    case north, south, east, west
+//    
+//    static func random() -> Direction {
+//        Direction.allCases.randomElement()!
+//    }
+//}
+//
+//let randomDirection = Direction.random()
+//print(randomDirection)
+
+//let age: Int! = nil
+
+//func username(for id: Int) -> String? {
+//    if id == 1 {
+//        return "Taylor Swift"
+//    } else {
+//        return nil
+//    }
+//}
+//
+//let user = username(for: 15) ?? "Anonymous"
+//
+//print(user)
+
+//let names = ["John", "Paul", "George", "Ringo"]
+////let names = [String]()
+//
+//let beatle = names.first?.uppercased()
+//
+//print(beatle)
+//print(beatle?.appending(" is a beatle."))
+
+//enum PasswordError: Error {
+//    case obvious
+//}
+//
+//func checkPassword(_ password: String) throws -> Bool {
+//    if password == "password" {
+//        throw PasswordError.obvious
+//    }
+//    
+//    return true
+//}
+//
+//if let result = try? checkPassword("password") {
+//    print("Result was \(result)")
+//} else {
+//    print("D'oh.")
+//}
+//
+//let isValidPassword = try! checkPassword("sekrit")
+//
+//print(isValidPassword)
+//print("OK!")
+
+//struct Person {
+//    var id: String
+//    
+//    init?(id: String) {
+//        if id.count == 9 {
+//            self.id = id
+//        } else {
+//            return nil
+//        }
+//    }
+//}
+//
+//struct Employee {
+//    var username: String
+//    var password: String
+//    
+//    init?(username: String, password: String) {
+//        guard password.count >= 8 else { return nil }
+//        guard password.lowercased() != "password" else { return nil }
+//        
+//        self.username = username
+//        self.password = password
+//    }
+//}
+//
+//let tim = Employee(username: "TimC", password: "app1e")
+//let craig = Employee(username: "CraigF", password: "ha1rf0rce0ne")
+//
+//print(tim)
+//print(craig)
+
+//class Animal { }
+//class Fish: Animal { }
+//
+//class Dog: Animal {
+//    func makeNoise() {
+//        print("Woof!")
+//    }
+//}
+//
+//let pets = [Fish(), Dog(), Fish(), Dog()]
+//
+//for pet in pets {
+//    if let dog = pet as? Dog {
+//        dog.makeNoise()
+//    }
+//}
+
+class Person {
+    var name = "Anonymous"
 }
 
-extension Identifiable {
-    func identify() {
-        print("My ID is \(id).")
+class Customer: Person {
+    var id = 12345
+}
+
+class Employee: Person {
+    var salary = 50_000
+}
+
+let customer = Customer()
+let employee = Employee()
+let people = [customer, employee]
+
+for person in people {
+    if let customer = person as? Customer {
+        print("I'm a customer, with id \(customer.id)")
+    } else if let employee = person as? Employee {
+        print("I'm an employee, earning $\(employee.salary)")
     }
+    
+    print("And my name is \(person.name)")
 }
-
-struct User: Identifiable {
-    var id: String
-}
-
-let eacardenase = User(id: "eacardenase")
-eacardenase.identify()
 
 //func printName(name: String) -> Void {
 //    print(name)
